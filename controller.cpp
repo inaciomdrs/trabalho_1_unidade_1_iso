87cd4580363e1ec247ca1a78560a2960c8303cac
inaciomdrs <boyinacio@gmail.com>
2015-03-11 23:05:02 -0300

    Pequenas mudanças em main e controller para garantir que tudo funcione corretamente

--- controller.cpp
+++ controller.cpp
@@ -15,6 +15,11 @@ Output_Interface* process(Input_Interface *input){
     Time_Register* for_quicksort  = NULL;
     Time_Register* for_bubblesort = NULL;
 
+    Output_Interface saida_quicksort, saida_bubblesort;
+    Output_Interface *output = new Output_Interface[2];
+    output[0].length = 0;
+    output[1].length = 0;
+
     init_Time_Register(for_quicksort,for_bubblesort,input->algorithm_selected,time_register_list_size);
 
     for (int vec_size = range->start, TR_iterator = 0; vec_size <= range->end; vec_size += range->pass, TR_iterator++) {
@@ -23,16 +28,14 @@ Output_Interface* process(Input_Interface *input){
 
         if(for_quicksort){
             runForAnAlgorithm("quicksort",quicksort,vetores,vec_size,for_quicksort[TR_iterator],total_time);
-//            std::cout << "(QUICK) Tempo Parcial: " << for_quicksort[TR_iterator].tempo_execucao << "\n";
+            //            std::cout << "(QUICK) Tempo Parcial: " << for_quicksort[TR_iterator].tempo_execucao << "\n";
         }
 
         if(for_bubblesort){
             runForAnAlgorithm("bubblesort",bubblesort,vetores,vec_size,for_bubblesort[TR_iterator],total_time);
-//            std::cout << "(BUBBLE) Tempo Parcial: " << for_bubblesort[TR_iterator].tempo_execucao << "\n";
+            //            std::cout << "(BUBBLE) Tempo Parcial: " << for_bubblesort[TR_iterator].tempo_execucao << "\n";
         }
     }
-
-//    std::cout << "Tempo Total: " << total_time << "\n";
 
     if(for_quicksort){
 
@@ -40,7 +43,10 @@ Output_Interface* process(Input_Interface *input){
             for_quicksort[i].percentage = (for_quicksort[i].tempo_execucao / total_time) * 100;
         }
 
-//        printVector(for_quicksort,time_register_list_size);
+        saida_quicksort = translate(for_quicksort,time_register_list_size);
+        saida_quicksort.length = time_register_list_size;
+        output[0] = saida_quicksort;
+
     }
 
     if(for_bubblesort){
@@ -49,20 +55,11 @@ Output_Interface* process(Input_Interface *input){
             for_bubblesort[i].percentage = (for_bubblesort[i].tempo_execucao / total_time) * 100;
         }
 
-//        printVector(for_bubblesort,time_register_list_size);
+        saida_bubblesort = translate(for_bubblesort,time_register_list_size);
+        saida_bubblesort.length = time_register_list_size;
+        output[1] = saida_bubblesort;
+
     }
-
-    std::cout << "====================================================================";
-
-    Output_Interface saida_quicksort = translate(for_quicksort,time_register_list_size);
-    Output_Interface saida_bubblesort = translate(for_bubblesort,time_register_list_size);
-
-    saida_quicksort.length = time_register_list_size;
-    saida_bubblesort.length = time_register_list_size;
-
-    Output_Interface output[2];
-    output[0] = saida_quicksort;
-    output[1] = saida_bubblesort;
 
     return output;
 
--- main.cpp
+++ main.cpp
@@ -1,47 +1,62 @@
 #include "mainwindow.h"
-#include "controller.h"
+#include "controller.h" // Inclua isso no começo do arquivo da interface gráfica
 
 #include <QApplication>
 #include <string>
 
 int main(int argc, char *argv[])
 {
-    //    QApplication a(argc, argv);
-    //    MainWindow w;
-    //    w.show();
-
-    //    return a.exec();
-
+    // Por detrás da interface gráfica, defina essa variável para guardar nela os valores de entrada
+    // das caixas de texto
     Input_Interface I;
-    I.algorithm_selected = 3;
-    I.iteracoes = 10;
-    I.range = "25000:25000:100000";
 
+
+    // Aqui é o seguinte: como vai ter duas checkboxs (uma pro bubblesort e outra pro quicksort)
+    // tu vai fazer o seguinte: se só o quick tiver sido selecionado, então vai I.algorithm_selected
+    // vai ser 1. Se só o bubble tiver sido selecionado, então vai ser 2. Se os dois tiverem sido
+    // selecionados, então vai ser 3.
+    I.algorithm_selected = 3;
+
+    // No código com interface gráfica, aqui seria algo do tipo:
+    // I.iteracoes = ui->numIteracoes->intValue()
+    // Considerando que tem um textbox de nome 'numIteracoes'
+    I.iteracoes = 10;
+
+    // Mesma coisa pra cá
+    I.range = "10:10:50";
+
+    // esse método process pega uma Input_Interface, faz o muido todo de medição, ordenação
+    // etc. etc. e devolve os resultados todos prontins, só os vetores de int e double show
+    // de bola, todos guardados num ponteiro de 'Output_Interface'
+
+    // Na verdade, na verdade, a saída...
     Output_Interface* saidas = process(&I);
 
     Output_Interface saida_quicksort = saidas[0];
     Output_Interface saida_bubblesort = saidas[1];
 
-    std::cout << "\nIMPRIMINDO SAÍDA BRUTA PARA QUICKSORT\n";
-    for (int i = 0; i < saida_quicksort.length; ++i) {
-        std::cout << saida_quicksort.tamanhos_vetores[i] << " "
-                  << saida_quicksort.tempo[i] << " "
-                  << saida_quicksort.porcentagem[i] << " "
-                  << saida_quicksort.desvio_padrao_inferior[i] << " "
-                  << saida_quicksort.desvio_padrao_superior[i] << "\n";
+    if(saida_quicksort.length != 0) {
+        std::cout << "\nIMPRIMINDO SAÍDA BRUTA PARA QUICKSORT\n";
+        for (int i = 0; i < saida_quicksort.length; ++i) {
+            std::cout << saida_quicksort.tamanhos_vetores[i] << " "
+                      << saida_quicksort.tempo[i] << " "
+                      << saida_quicksort.porcentagem[i] << " "
+                      << saida_quicksort.desvio_padrao_inferior[i] << " "
+                      << saida_quicksort.desvio_padrao_superior[i] << "\n";
+        }
     }
 
-    std::cout << "\nIMPRIMINDO SAÍDA BRUTA PARA QUICKSORT\n";
-    for (int i = 0; i < saida_bubblesort.length; ++i) {
-        std::cout << saida_quicksort.tamanhos_vetores[i] << " "
-                  << saida_bubblesort.tempo[i] << " "
-                  << saida_bubblesort.porcentagem[i] << " "
-                  << saida_bubblesort.desvio_padrao_inferior[i] << " "
-                  << saida_bubblesort.desvio_padrao_superior[i] << "\n";
+    if(saida_bubblesort.length != 0){
+        std::cout << "\nIMPRIMINDO SAÍDA BRUTA PARA BUBBLESORT\n";
+        for (int i = 0; i < saida_bubblesort.length; ++i) {
+            std::cout << saida_bubblesort.tamanhos_vetores[i] << " "
+                      << saida_bubblesort.tempo[i] << " "
+                      << saida_bubblesort.porcentagem[i] << " "
+                      << saida_bubblesort.desvio_padrao_inferior[i] << " "
+                      << saida_bubblesort.desvio_padrao_superior[i] << "\n";
+        }
     }
 
     std::cout << "FIM\n";
-
-
 
 }
